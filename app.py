9c6e559c25e39336f0bcc1ef5b704703298125e5
Mohamed Aziz Ben Nessir <70135168+Mohamed-Aziz-Ben-Nessir@users.noreply.github.com>
2021-12-05 23:59:17 +0100

    Add files via upload

---
+++ app.py
@@ -0,0 +1,24 @@
+import uvicorn
+from fastapi import FastAPI, Request, Form, Depends, UploadFile, File
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+from schemas import Form
+
+app = FastAPI()
+
+templates = Jinja2Templates(directory="templates")
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+@app.get('/', response_class=HTMLResponse)
+def get_form(request: Request):
+   return templates.TemplateResponse("form.html", {"request": request})
+
+@app.post('/', response_class=HTMLResponse)
+def post_form(request: Request, form_data: Form = Depends(Form.as_form)):
+    print(form_data)
+    return templates.TemplateResponse("form.html", {"request": request})
+
+
+if __name__ == '__main__':
+    uvicorn.run(app) 
---
+++ schemas.py
@@ -0,0 +1,41 @@
+from fastapi import Form, File, UploadFile
+from pydantic import BaseModel
+
+class Form(BaseModel):
+    title : str
+    name1 : str
+    pos1 : str
+    name2 : str
+    pos2 : str
+    date : str
+    time : str
+    locaton : str
+    pic1 : UploadFile
+    pic2 : UploadFile
+
+    @classmethod
+    def as_form(
+        cls,
+        title: str = Form(...),
+        name1: str = Form(...),
+        pos1: str = Form(...),
+        name2: str = Form(...),
+        pos2: str = Form(...),
+        date: str = Form(...),
+        time: str = Form(...),
+        location: str = Form(...),
+        pic1: UploadFile = File(...),
+        pic2: UploadFile = File(...)
+    ):
+        return cls(
+            title=title,
+            name1=name1,
+            pos1=pos1,
+            name2=name2,
+            pos2=pos2,
+            date=date,
+            time=time,
+            location=location,
+            pic1=pic1,
+            pic2=pic2
+        )
